f4d958213312506f38a1c1dd3b22fa869c47acef
Ziv Perry <ziv.perry@zoominfo.com>
2025-12-22 18:48:47 +0200

    deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 ng build leveler --base-href=/ngapps/leveler/
 rm -rf docs/leveler
 cp -r dist/leveler/browser docs/leveler
+
+ng build compass --base-href=/ngapps/compass/
+rm -rf docs/compass
+cp -r dist/compass/browser docs/compass
