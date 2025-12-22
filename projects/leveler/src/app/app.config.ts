abd853eff83b326c170134ebfc3e2445ba05048c
Ziv Perry <ziv.perry@zoominfo.com>
2025-12-22 13:03:14 +0200

    chore: pwa added

--- projects/leveler/src/app/app.config.ts
+++ projects/leveler/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import {ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, isDevMode} from '@angular/core';
 import {MatIconRegistry} from '@angular/material/icon';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideAppInitializer(() => {
       inject(MatIconRegistry).setDefaultFontSetClass('material-symbols-outlined');
-    }),
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 };
